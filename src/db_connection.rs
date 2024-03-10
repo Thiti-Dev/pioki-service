d9e43fb59ad77c07e77c4277799c6f05350b42a1
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-03-10 18:09:22 +0700

    chore: creates get_connection_pool

---
+++ src/db_connection.rs
@@ -0,0 +1,18 @@
+use r2d2::Pool;
+use diesel::PgConnection;
+use diesel::r2d2::ConnectionManager;
+use std::env;
+use log;
+
+pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+pub fn get_connection_pool() -> DbPool {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    log::info!("Connecting to the database.");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+
+    Pool::builder()
+        .max_size(5)
+        .build(manager)
+        .expect("Failed to create connection Pool.")
+}
