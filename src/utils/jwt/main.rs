5a3c9cd0891fa9d828cc54a959fb7df0459b44db
Thiti-Dev <thiti.mwk.main@gmail.com>
2024-03-10 18:09:47 +0700

    creates jwt-utils -> verify_jwt

--- src/utils/jwt/main.rs
+++ src/utils/jwt/main.rs
@@ -9,8 +9,8 @@ pub struct Claims {
 
 pub fn verify_jwt(token: &str, secret: &str) -> Result<Claims, jsonwebtoken::errors::Error> {
     let decoding_key = DecodingKey::from_secret(secret.as_ref());
-    let mut validation = Validation::new(Algorithm::HS256);
-    validation.leeway = 5;
+    let validation = Validation::new(Algorithm::HS256);
+    // validation.leeway = 5;
     let token_data = decode::<Claims>(token, &decoding_key, &validation)?;
 
     Ok(token_data.claims)
